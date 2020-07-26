4f8092ba36f6788c9108a11a682abee7149e9df0
yyuguang <790335255@qq.com>
2020-07-26 20:49:55 +0800

    07.Hystrix

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/lnzz/HystrixDashboardMain9001.java
@@ -0,0 +1,12 @@
+package com.lnzz;
+
+/**
+ * ClassName：HystrixDashboardMain9001
+ *
+ * @Author 冷暖自知
+ * @Date 2020/7/26 20:18
+ * @Version 1.0
+ * @Description
+ */
+public class HystrixDashboardMain9001 {
+}
